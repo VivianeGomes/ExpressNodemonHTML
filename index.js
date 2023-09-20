119747eaf1a87ea04dcb9a89eac2bd52a5ec478f
Viviane <viviane.sp13@gmail.com>
2023-09-20 00:24:41 -0300

    limpar cache

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.use(express.static(path.resolve(__dirname, "./src/public")));
 
 app.use("/", routes);
 
+app.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
 // Inicia o servidor na porta 5000
 app.listen(5000, () => {
   console.log("Servidor rodando na porta 5000");
