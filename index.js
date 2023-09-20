0bae951c6f014f35348b9c6572a03ccdcf261bc3
Viviane <viviane.sp13@gmail.com>
2023-09-20 00:10:02 -0300

    fix: img

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const routes = require('./src/routes');
+const path = require("path");
+const routes = require("./src/routes");
 
-app.use('/', routes);
+app.use(express.static(path.resolve(__dirname, "./src/public")));
+
+app.use("/", routes);
 
 // Inicia o servidor na porta 5000
 app.listen(5000, () => {
-  console.log('Servidor rodando na porta 5000');
+  console.log("Servidor rodando na porta 5000");
 });
